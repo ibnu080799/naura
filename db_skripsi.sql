c17de380a485cf0d4df0d38493f976e5215808f1
amru09 <amrufauzihk@gmail.com>
2022-02-13 22:28:32 +0800

    update database

--- db_skripsi.sql
+++ db_skripsi.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.4.21-MariaDB - mariadb.org binary distribution
+-- Server version:               10.4.18-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             11.3.0.6295
+-- HeidiSQL Version:             11.0.0.5919
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,12 +10,26 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Dumping database structure for db_skripsi
 CREATE DATABASE IF NOT EXISTS `db_skripsi` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `db_skripsi`;
+
+-- Dumping structure for table db_skripsi.tb_kebutuhan
+CREATE TABLE IF NOT EXISTS `tb_kebutuhan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_mitra` char(50) DEFAULT NULL,
+  `judul` varchar(50) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table db_skripsi.tb_kebutuhan: ~0 rows (approximately)
+/*!40000 ALTER TABLE `tb_kebutuhan` DISABLE KEYS */;
+INSERT INTO `tb_kebutuhan` (`id`, `id_mitra`, `judul`, `deskripsi`) VALUES
+	(4, '1', 'Sopir', 'Dsess');
+/*!40000 ALTER TABLE `tb_kebutuhan` ENABLE KEYS */;
 
 -- Dumping structure for table db_skripsi.tb_mitra
 CREATE TABLE IF NOT EXISTS `tb_mitra` (
@@ -54,12 +68,18 @@ CREATE TABLE IF NOT EXISTS `tb_pelamar` (
   `password` varchar(250) DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table db_skripsi.tb_pelamar: ~1 rows (approximately)
+-- Dumping data for table db_skripsi.tb_pelamar: ~7 rows (approximately)
 /*!40000 ALTER TABLE `tb_pelamar` DISABLE KEYS */;
 INSERT INTO `tb_pelamar` (`id`, `nama`, `jkl`, `tempat_lahir`, `tgl_lahir`, `alamat`, `no_hp`, `email`, `file_name`, `username`, `password`, `status`) VALUES
-	(1, 'naura', 'P', 'mkm', '2022-01-10', 'asdsa', '09876', 'adsa', 'oih', 'nor', '57c7d11cd49333e3f722204c63016da9', 1);
+	(1, 'naura', 'P', 'mkm', '2022-01-10', 'asdsa', '09876', 'adsa', 'oih', 'nor', '57c7d11cd49333e3f722204c63016da9', 1),
+	(2, 'Amru', 'L', 'Makassar', '2022-02-16', 'Bumi Permata Sudiang Blok A2 No 14,', '+6281247352852', 'amrufauzihk@gmail.com', 'assets/filedata/file-2022-02-13 10-36-50-cover.pdf', 'amru', 'amru', 0),
+	(3, 'Amru', 'L', 'Makassar', '2022-02-16', 'Bumi Permata Sudiang Blok A2 No 14,', '+6281247352852', 'amrufauzihk@gmail.com', 'assets/filedata/file-2022-02-13 10-38-30-cover.pdf', 'amru', 'amru', 0),
+	(4, 'Amru', 'L', 'Makassar', '2022-02-16', 'Bumi Permata Sudiang Blok A2 No 14,', '+6281247352852', 'amrufauzihk@gmail.com', 'assets/filedata/file-2022-02-13 10-40-44-cover.pdf', 'amru', 'amru', 0),
+	(5, 'Amru', 'L', 'Makassar', '2022-02-13', 'Bumi Permata Sudiang Blok A2 No 14,', '+6281247352852', 'amrufauzihk@gmail.com', 'assets/filedata/file-2022-02-13 10-41-29-cover.pdf', 'amru', 'amru', 0),
+	(6, 'Dina', 'P', 'Msaka', '2001-02-02', 'Makassar', '08229312321', 'amrufauzihk@gmail.com', 'assets/filedata/file-2022-02-13 10-42-40-cover.pdf', 'amru', 'amru', 0),
+	(7, 'Amru', 'L', 'Makassar', '2022-02-11', 'Bumi Permata Sudiang Blok A2 No 14,', '+6281247352852', 'amrufauzihk@gmail.com', 'assets/filedata/file-2022-02-13 11-05-50-cover.pdf', 'amru', '5890b9947c43786471fc0cbd4d5f00f1', 1);
 /*!40000 ALTER TABLE `tb_pelamar` ENABLE KEYS */;
 
 -- Dumping structure for table db_skripsi.tb_profil
@@ -73,13 +93,12 @@ CREATE TABLE IF NOT EXISTS `tb_profil` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table db_skripsi.tb_profil: ~1 rows (approximately)
+-- Dumping data for table db_skripsi.tb_profil: ~0 rows (approximately)
 /*!40000 ALTER TABLE `tb_profil` DISABLE KEYS */;
 INSERT INTO `tb_profil` (`id`, `nama`, `email`, `username`, `password`, `status`) VALUES
 	(1, 'admin', 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
 /*!40000 ALTER TABLE `tb_profil` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
